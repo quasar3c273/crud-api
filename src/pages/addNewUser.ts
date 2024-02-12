ed175493d2cfc72fad6fb17d2b3adf7f47df00ea
Lapitskiy <hiimisha@mail.ru>
2024-02-13 02:57:46 +0300

    feat: add types

--- src/pages/addNewUser.ts
+++ src/pages/addNewUser.ts
@@ -3,7 +3,11 @@ import {UserInterface} from "../types/User";
 import {v4 as uuid} from "uuid";
 import {users} from "../consts";
 
-const addNewUser = (response: ServerResponse, userInfo) => {
+const addNewUser = (response: ServerResponse, userInfo: {
+  username: string;
+  age: number;
+  hobbies: string[];
+}) => {
   const newUser: UserInterface = {
     id: uuid(),
     username: userInfo.username,
@@ -16,6 +20,6 @@ const addNewUser = (response: ServerResponse, userInfo) => {
   response.statusCode = 201;
   response.setHeader('Content-Type', 'application/json');
   response.end(JSON.stringify(newUser));
-}
+};
 
 export default addNewUser
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import {validate} from "uuid";
-import {ServerResponse} from "http";
 
 export const validateIdByUuid = (userId: any): boolean => {
   return validate(userId);
@@ -7,17 +6,17 @@ export const validateIdByUuid = (userId: any): boolean => {
 export const isTrueHobbiesFormat = (hobbiesArray: Array<any>): boolean => {
   return hobbiesArray.every((hobby) => typeof hobby === 'string');
 };
-export const isNotFullUserInfo = (userInfo): boolean => {
-  return !userInfo.username ||
+export const isNotFullUserInfo = (userInfo: {
+  username?: string;
+  age?: number;
+  hobbies?: string[];
+}): boolean => {
+  return (
+    !userInfo.username ||
     !userInfo.age ||
     !userInfo.hobbies ||
     typeof userInfo.username !== 'string' ||
-    typeof userInfo.age !== 'number' ||
     !Array.isArray(userInfo.hobbies) ||
     !isTrueHobbiesFormat(userInfo.hobbies)
+  );
 };
-export const getResponseInfo = (code: number, response: ServerResponse, reqInfo: string) => {
-  response.statusCode = 400;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end(reqInfo);
-}
